8473e2c3c763739e623a6461f781e361a9860dbd
rbujny <radoslaw.bujny@gmail.com>
2023-05-03 23:55:55 +0200

    Next commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let angle_down = document.getElementById("angle-down")
 let angle_up = document.getElementById("angle-up")
 let main_list = document.getElementById("main_list")
+let photos = document.getElementById("photos")
+
 main_list.style.display = "none"
 angle_up.style.display = "none"
 function showMenu()
@@ -8,10 +10,12 @@ function showMenu()
     angle_down.style.display = 'none'
     main_list.style.display = ""
     angle_up.style.display = ""
+    photos.style.flexDirection = "column"
 }
 function hideMenu()
 {
     angle_down.style.display = ''
     main_list.style.display = "none"
     angle_up.style.display = "none"
+    photos.style.flexDirection = "row"
 }
